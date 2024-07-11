c4a9060194dec77437612aefdc6a70815d8df678
ASUS <5646krupaparmar@gmail.com>
2024-07-11 22:50:32 +0530

    krupa krishu KRISHNA cd crad

--- lib/screens/task-1.5/view/quotePage/quotePage.dart
+++ lib/screens/task-1.5/view/quotePage/quotePage.dart
@@ -1,34 +1,56 @@
 import 'package:avd_ch_1/screens/task-1.5/model/quoteModel/quoteModel.dart';
 import 'package:avd_ch_1/screens/task-1.5/provider/quotePageProvider/quotePageProvider.dart';
+import 'package:avd_ch_1/utils/globle/globle.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-QuoteModel? quoteModel;
-
+QuoteModel quoteModelNew = QuoteModel();
 
 class quotePage extends StatelessWidget {
   const quotePage({super.key});
 
   @override
-
   Widget build(BuildContext context) {
- //   quoteModelText=QuoteModel.toList(l1:QuoteList)
     quoteProvider quoteProviderTrue =
         Provider.of<quoteProvider>(context, listen: true);
     quoteProvider quoteProviderFalse =
         Provider.of<quoteProvider>(context, listen: false);
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: quoteProviderTrue.QuotesList.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(quoteProviderTrue.QuotesList[index]['quote']),
-          subtitle: Text(quoteProviderTrue.QuotesList[index]['author']),
-          trailing: GestureDetector(
-          onDoubleTap: () {
-            quoteProviderFalse.removeQuotes(index);
-          }
 
-          ,child: Icon(Icons.delete,color: Colors.red,)),
+    quoteModelNew = QuoteModel.toList(QuotesList);
+    GlobalKey<FormState> quoteKey = GlobalKey();
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.teal.shade400,
+        title: Text(
+          "Quotes",
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22),
+        ),
+      ),
+      body:
+      ListView.builder(
+        itemCount: quoteModelNew.QuoteListNew.length,
+        itemBuilder: (context, index) => Card(
+          color: Colors.grey.shade100,
+          child: ListTile(
+            title: Text(
+              quoteModelNew.QuoteListNew[index].quote.toString(),
+            ),
+            subtitle: Text(
+              quoteModelNew.QuoteListNew[index].quote.toString(),
+            ),
+            trailing: GestureDetector(
+                onDoubleTap: () {
+                  quoteProviderFalse.removeQuotes(index);
+                },
+                child: Icon(
+                  Icons.delete,
+                  color: Colors.teal,
+                )),
+
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -37,82 +59,110 @@ class quotePage extends StatelessWidget {
               context: context,
               builder: (BuildContext context) {
                 return AlertDialog(
-                    title: Column(
-                  children: [
-                    Text("Enter the quotes"),
-                    SizedBox(
-                      height: 20,
+                  backgroundColor: Colors.teal,
+                  title: Form(
+                    key: quoteKey,
+                    child: Column(
+                      children: [
+                        Text(
+                          "Enter the quotes",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          style: const TextStyle(color: Colors.white),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Field Must be Required !';
+                            }
+                            return null;
+                          },
+                          controller: txtQuote,
+                          decoration: InputDecoration(
+                              label: Text(
+                                "Enter the your quotes",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(color: Colors.white)),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(color: Colors.white))
+                              // labelText:"Life isn’t about getting and having, it’s about giving and being.",
+
+                              ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Field Must be Required !';
+                            }
+                            return null;
+                          },
+                          controller: txtAuthor,
+                          decoration: InputDecoration(
+                              label: Text(
+                                "Enter the author",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(color: Colors.white)),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(10),
+                              )
+                              // labelText:"Life isn’t about getting and having, it’s about giving and being.",
+
+                              ),
+                        ),
+                      ],
                     ),
-                    TextField(
-                      controller: txtQuote,
-
-                      decoration: InputDecoration(
-                          label: Text("Enter the your quotes"),
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                              borderSide: BorderSide(color: Colors.black)),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20))
-                          // labelText:"Life isn’t about getting and having, it’s about giving and being.",
-
-                          ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextField(
-                      controller: txtAuthor,
-                      decoration: InputDecoration(
-                          label: Text("Enter the your author"),
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(color: Colors.black)),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10))
-                          // labelText:"Life isn’t about getting and having, it’s about giving and being.",
-
-                          ),
-                    ),
-
-
-
-
-
-
-
-
-
-
-
-                  ],
-
-                ),
+                  ),
                   actions: [
-                    TextButton(onPressed: () {
-                      quoteProviderFalse.addData();
-                      quoteProviderFalse.addQuotes();
-                    }, child: Text('Save')),
+                    TextButton(
+                        onPressed: () {
+                          quoteProviderFalse.addData();
+                          quoteProviderFalse.addQuotes();
+                        },
+                        child: Text('Save',
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),)),
                     TextButton(
                       onPressed: () {
                         Navigator.of(context).pop();
                       },
                       child: Text(
                         'Back',
-                        style: TextStyle(color: Colors.black87, fontSize: 20),
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
                       ),
                     ),
-
-
                   ],
                 );
-
-              }
-              );
-
+              });
         },
         child: Icon(Icons.add),
       ),
     );
   }
 }
-QuoteModel quoteModelText=QuoteModel();
